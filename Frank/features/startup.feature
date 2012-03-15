1afc8fa2d71a88e976a1e86fe1a9c0e2da6c25d1
Gregory Long <gregorydavidlong@gmail.com>
2012-03-15 12:25:48 +1100

    Initial commit

---
+++ Frank/features/startup.feature
@@ -0,0 +1,8 @@
+Feature: 
+  As a user
+  I want to be presented with a start-up view
+  So I can access the application's features
+  
+  Scenario: Starting the application
+    Given I launch the app
+	Then I should see the main view
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TwitterTest
+//
+//  Created by Gregory David Long on 15/03/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
